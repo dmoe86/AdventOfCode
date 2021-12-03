1551d98f6a08735964fcede29604e9eb44e714ea
David Moe <david.moe86@gmail.com>
2021-12-02 21:15:21 -0600

    playing with jupyer integrations

--- day1/day1-2.py
+++ day1/day1-2.py
@@ -1,6 +1,8 @@
+#%%
 inputFile = open("./1-1_inputs.txt")
 lines = inputFile.readlines()
 nums = [ int(i) for i in lines ]
+#%%
 counter = 0
 for i in range(1,len(nums)):
     if sum(nums[i:i+3]) > sum(nums[i-1:i+2]):
